065812fbc04f73b36f7efab7f95f2e25f14fdc3d
Pranta Hossen <83763254+PrantaHossen@users.noreply.github.com>
2022-06-07 20:49:16 +0600

    Add files via upload

---
+++ Binarysearch.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Number ";
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+
+    int key;
+    cin>>key;
+    int flag;
+    int l=0;
+    int r=n-1;
+
+    while(l<=r)
+    { 
+        int mid=(l+r)/2;
+        if(key==arr[mid])
+        {
+            cout<<"Number Found";
+            flag++;
+            break;
+        }
+        else if(key>arr[mid])
+        {
+            l=mid+1;
+        }
+        else
+        {
+           r=mid-1;
+        }
+    }
+    if(flag==0)
+    {
+        cout<<"Number Not Founded";
+    }
+}
